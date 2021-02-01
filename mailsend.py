c7ed90dac8af1f986b54c2f880bedef46b92cb6b
Jonathon Reinhart <Jonathon.Reinhart@gmail.com>
2021-01-31 22:21:52 -0500

    Refactor send_email() function and switch from print to logging

--- mailsend.py
+++ mailsend.py
@@ -3,6 +3,9 @@ import smtplib
 import dns.resolver
 import sys
 from email.message import EmailMessage
+import logging
+
+logger = logging.getLogger(__name__)
 
 def get_mailservers(domain):
     mxresult = dns.resolver.query(domain, 'MX')
@@ -10,29 +13,57 @@ def get_mailservers(domain):
         yield r.exchange.to_text().rstrip('.')
 
 
+def send_email(mailsrv, msg):
+    with smtplib.SMTP(mailsrv) as smtp:
+        logger.debug("Issuing STARTTLS")
+        r = smtp.starttls()
+        logger.debug("STARTTLS response: {}".format(r))
+
+        logger.debug("Issuing EHLO (again)")
+        r = smtp.ehlo()
+        logger.debug("EHLO response: {}".format(r))
+
+        logger.debug("Issuing NOOP")
+        r = smtp.noop()
+        logger.debug("NOOP response: {}".format(r))
+
+        logger.debug("Sending message")
+        smtp.send_message(msg)
+
+        logger.debug("Issuing QUIT")
+        r = smtp.quit()
+        logger.debug("QUIT response: {}".format(r))
+
+
 def parse_args():
+    DEFAULT_LOGLEVEL = 'WARNING'
+
     from argparse import ArgumentParser
     ap = ArgumentParser()
     ap.add_argument('--from', dest='mailfrom', required=True)
     ap.add_argument('--to', dest='mailto', required=True)
     ap.add_argument('--subject')
+    ap.add_argument('--loglevel', type=str.upper,
+            choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
+            default=DEFAULT_LOGLEVEL,
+            help='Set the logging level (default: {})'.format(DEFAULT_LOGLEVEL))
     return ap.parse_args()
 
 
 def main():
     args = parse_args()
+    logging.basicConfig(level=args.loglevel)
 
     # Build the message
     msg = EmailMessage()
     msg.set_content(sys.stdin.read())
 
-    msg['Subject'] = args.subject
     msg['From'] = args.mailfrom
     msg['To'] = args.mailto
+    if args.subject:
+        msg['Subject'] = args.subject
 
-    print("\nMessage:\n")
-    print(msg)
-    print()
+    logger.info("Message:\n{}".format(msg))
 
     # Look up mailservers
     username, domain = args.mailto.split('@')
@@ -40,22 +71,13 @@ def main():
 
     # Try to send!
     for hostname in mailsrvs:
-        print("Trying mailserver {}".format(hostname))
+        logger.info("Trying mailserver {}".format(hostname))
 
-        with smtplib.SMTP(hostname) as smtp:
-            print("  Starting TLS:")
-            print(smtp.starttls())
-            print("  EHLO again:")
-            print(smtp.ehlo())
-            print("  NOOP:")
-            print(smtp.noop())
-
-            print("  Sending message!")
-            print(smtp.send_message(msg))
-
-            print("  Quit:")
-            print(smtp.quit())
-
-        break
+        try:
+            send_email(hostname, msg)
+        except Exception:  # TODO: Narrow
+            logger.exception("Error sending message")
+        else:
+            break
 
 main()
