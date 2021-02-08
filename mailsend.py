31ea73a87a98674f654cc6d40fe760c3566d3699
Jonathon Reinhart <Jonathon.Reinhart@gmail.com>
2021-02-08 00:39:50 -0500

    Add --tls mode and --no-verify options

--- mailsend.py
+++ mailsend.py
@@ -6,8 +6,18 @@ from email.message import EmailMessage
 import logging
 import socket
 import ssl
+from enum import Enum
 
 logger = logging.getLogger(__name__)
+
+class TLSMode(Enum):
+    NONE = 'none'
+    STARTTLS = 'starttls'
+    ALWAYS = 'always'
+
+    def __str__(self):
+        return self.value
+
 
 def get_mailservers(domain):
     mxresult = dns.resolver.query(domain, 'MX')
@@ -15,11 +25,16 @@ def get_mailservers(domain):
         yield r.exchange.to_text().rstrip('.')
 
 
-def send_email(mailsrv, message, sslctx=None, username=None, password=None):
+def send_email(mailsrv, message, tls_mode=TLSMode.NONE, sslctx=None, username=None, password=None):
+
+    if tls_mode == TLSMode.ALWAYS:
+        raise NotImplementedError()
+
     with smtplib.SMTP(mailsrv) as smtp:
-        logger.debug("Issuing STARTTLS")
-        r = smtp.starttls(context=sslctx)
-        logger.debug("STARTTLS response: {}".format(r))
+        if tls_mode == TLSMode.STARTTLS:
+            logger.debug("Issuing STARTTLS")
+            r = smtp.starttls(context=sslctx)
+            logger.debug("STARTTLS response: {}".format(r))
 
         if username is not None:
             logger.debug("Logging in")
@@ -35,6 +50,7 @@ def send_email(mailsrv, message, sslctx=None, username=None, password=None):
 
 def parse_args():
     DEFAULT_LOGLEVEL = 'WARNING'
+    DEFAULT_TLS_MODE = 'starttls'
 
     from argparse import ArgumentParser
     ap = ArgumentParser()
@@ -46,15 +62,37 @@ def parse_args():
             default=DEFAULT_LOGLEVEL,
             help='Set the logging level (default: {})'.format(DEFAULT_LOGLEVEL))
 
+    # Server selection
     grp = ap.add_mutually_exclusive_group()
     grp.add_argument('--server', default="localhost",
             help="SMTP server to connect to")
     grp.add_argument('--resolve', action="store_true",
             help="Find SMTP server by recipient domain MX record")
 
-    ap.add_argument('--auth-username')
+    # TLS Settings
+    ap.add_argument('--tls',
+            choices = TLSMode,
+            default=DEFAULT_TLS_MODE,
+            type=TLSMode,
+            help="TLS mode (default: {})".format(DEFAULT_TLS_MODE))
+    ap.add_argument('--no-verify', action='store_true',
+            help="Don't verify TLS server certificate (for testing only!)")
+
+    # Authentication
+    ap.add_argument('--auth-username',
+            help="User to authenticate as (will prompt for password)")
 
     return ap.parse_args()
+
+
+def get_ssl_context(args):
+    sslctx = ssl.create_default_context()
+
+    if args.no_verify:
+        sslctx.check_hostname = False
+        sslctx.verify_mode = ssl.CERT_NONE
+
+    return sslctx
 
 
 def main():
@@ -85,7 +123,6 @@ def main():
     else:
         mailsrvs = [args.server]
 
-    sslctx = ssl.create_default_context()
 
     # Try to send!
     for hostname in mailsrvs:
@@ -94,7 +131,8 @@ def main():
         try:
             send_email(mailsrv=hostname,
                        message=msg,
-                       sslctx=sslctx,
+                       tls_mode=args.tls,
+                       sslctx=get_ssl_context(args),
                        username=args.auth_username,
                        password=password,
                        )
